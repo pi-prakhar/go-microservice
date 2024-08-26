e1ae425f9e6ef51f28650b7a882e4028b347410d
Prakhar Awasthi <pawasthi961@gmail.com>
2024-08-26 16:20:19 +0530

    updated query file to make auth service work

--- query.sql
+++ query.sql
@@ -1,22 +1,55 @@
-create table users(
-id int primary key,
-email varchar(50) NOT NULL,
-first_name varchar(25),
-last_name varchar(25),
-password varchar(250) NOT NULL,
-active int,
-created_at time DEFAULT CURRENT_TIMESTAMP,
-updated_at time DEFAULT CURRENT_TIMESTAMP)
+CREATE SEQUENCE public.user_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
 
-alter table users
-rename column update_at to updated_at
+ALTER TABLE public.user_id_seq OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+CREATE TABLE public.users (
+    id integer DEFAULT nextval('public.user_id_seq'::regclass) NOT NULL,
+    email character varying(255),
+    first_name character varying(255),
+    last_name character varying(255),
+    password character varying(60),
+    user_active integer DEFAULT 0,
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone
+);
 
 
-insert into users
-(id, email, first_name, password, active)
-values
-(1, 'admin@example.com', 'admin', 'verysecret', 0);
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- Name: user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.user_id_seq', 1, true);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+INSERT INTO "public"."users"("email","first_name","last_name","password","user_active","created_at","updated_at")
+VALUES
+(E'admin@example.com',E'Admin',E'User',E'$2a$12$1zGLuYDDNvATh4RA4avbKuheAMpb1svexSzrQm7up.bnpwQHs0jNe',1,E'2022-03-14 00:00:00',E'2022-03-14 00:00:00');
+
 
 select * from users;
+DROP TABLE IF EXISTS users;
 select id, email, first_name, last_name, password, active, created_at, updated_at from users where email = 'admin@example.com';
+DISCARD ALL;
+
+alter table only public.users
+rename column user_active to active;
